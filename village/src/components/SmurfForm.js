6deb6abf295f14c6467f409557150c20dd968ce0
oyekunle-mark <oyekunlemac@gmail.com>
2019-06-14 09:15:10 +0100

    Refactored the form

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,57 +1,44 @@
-import React, { Component } from 'react';
+import React from "react";
+import Prop from "prop-types";
 
-class SmurfForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      age: '',
-      height: ''
-    };
-  }
-
-  addSmurf = event => {
-    event.preventDefault();
-    // add code to create the smurf using the api
-
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
-    });
-  }
-
-  handleInputChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
-  render() {
-    return (
-      <div className="SmurfForm">
-        <form onSubmit={this.addSmurf}>
-          <input
-            onChange={this.handleInputChange}
-            placeholder="name"
-            value={this.state.name}
-            name="name"
-          />
-          <input
-            onChange={this.handleInputChange}
-            placeholder="age"
-            value={this.state.age}
-            name="age"
-          />
-          <input
-            onChange={this.handleInputChange}
-            placeholder="height"
-            value={this.state.height}
-            name="height"
-          />
-          <button type="submit">Add to the village</button>
-        </form>
-      </div>
-    );
-  }
+export default function SmurfForm({
+  name,
+  age,
+  height,
+  handleChange,
+  handleSubmit
+}) {
+  return (
+    <div className="SmurfForm">
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          placeholder="name"
+          value={name}
+          name="name"
+        />
+        <input
+          onChange={handleChange}
+          placeholder="age"
+          value={age}
+          name="age"
+        />
+        <input
+          onChange={handleChange}
+          placeholder="height"
+          value={height}
+          name="height"
+        />
+        <button type="submit">Add to the village</button>
+      </form>
+    </div>
+  );
 }
 
-export default SmurfForm;
+SmurfForm.propTypes = {
+  name: Prop.string.isRequired,
+  age: Prop.string.isRequired,
+  height: Prop.string.isRequired,
+  handleChange: Prop.func.isRequired,
+  handleSubmit: Prop.func.isRequired
+};
