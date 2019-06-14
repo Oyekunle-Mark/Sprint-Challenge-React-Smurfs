4675a262b8100a201928b8e03c2aac353bc93905
oyekunle-mark <oyekunlemac@gmail.com>
2019-06-14 14:11:56 +0100

    Removed the reset file and applied it in a style tag

--- village/src/index.js
+++ village/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import styled from "styled-components";
 
 import App from "./App";
 
