ed3924e4db9129bf999672c79ca4e8881f49dfdb
oyekunle-mark <oyekunlemac@gmail.com>
2019-06-14 11:55:25 +0100

    Added more smurfs to the server data

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const port = 3333;
 
 const server = express();
@@ -16,34 +16,46 @@ const sendUserError = (msg, res) => {
 let smurfs = [
   {
     id: 0,
-    name: 'Brainey Smurf',
+    name: "Brainey Smurf",
     age: 200,
-    height: '8cm'
+    height: "8cm"
   },
   {
     id: 1,
-    name: 'Sleepy',
+    name: "Sleepy",
     age: 323,
-    height: '5cm'
+    height: "5cm"
   },
   {
     id: 2,
-    name: 'Papa Smurf',
+    name: "Papa Smurf",
     age: 542,
-    height: '7.62cm'
+    height: "7.62cm"
+  },
+  {
+    id: 3,
+    name: "Smurfette",
+    age: 122,
+    height: "12cm"
+  },
+  {
+    id: 4,
+    name: "Grumpy",
+    age: 234,
+    height: "9cm"
   }
 ];
-server.get('/smurfs', (req, res) => {
+server.get("/smurfs", (req, res) => {
   res.json(smurfs);
 });
-let smurfId = 3;
+let smurfId = 5;
 
-server.post('/smurfs', (req, res) => {
+server.post("/smurfs", (req, res) => {
   const { name, age, height } = req.body;
   const newSmurf = { name, age, height, id: smurfId };
   if (!name || !age || !height) {
     return sendUserError(
-      'Ya gone did smurfed! Name/Age/Height are all required to create a smurf in the smurf DB.',
+      "Ya gone did smurfed! Name/Age/Height are all required to create a smurf in the smurf DB.",
       res
     );
   }
@@ -62,7 +74,7 @@ server.post('/smurfs', (req, res) => {
   res.json(smurfs);
 });
 
-server.put('/smurfs/:id', (req, res) => {
+server.put("/smurfs/:id", (req, res) => {
   const { id } = req.params;
   const { name, age, height } = req.body;
   const findSmurfById = smurf => {
@@ -70,7 +82,7 @@ server.put('/smurfs/:id', (req, res) => {
   };
   const foundSmurf = smurfs.find(findSmurfById);
   if (!foundSmurf) {
-    return sendUserError('No Smurf found by that ID', res);
+    return sendUserError("No Smurf found by that ID", res);
   } else {
     if (name) foundSmurf.name = name;
     if (age) foundSmurf.age = age;
@@ -79,7 +91,7 @@ server.put('/smurfs/:id', (req, res) => {
   }
 });
 
-server.delete('/smurfs/:id', (req, res) => {
+server.delete("/smurfs/:id", (req, res) => {
   const { id } = req.params;
   const foundSmurf = smurfs.find(smurf => smurf.id == id);
 
@@ -88,7 +100,7 @@ server.delete('/smurfs/:id', (req, res) => {
     smurfs = smurfs.filter(smurf => smurf.id != id);
     res.status(200).json(smurfs);
   } else {
-    sendUserError('No smurf by that ID exists in the smurf DB', res);
+    sendUserError("No smurf by that ID exists in the smurf DB", res);
   }
 });
 
